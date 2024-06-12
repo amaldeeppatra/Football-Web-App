b08a7168916a46d3ae9b1591a2f77e50b475755e
Vaibhdev <157033146+Vaibhdev@users.noreply.github.com>
2024-06-12 23:04:03 +0530

    full  news
     zdnfvn

--- src/pages/Cardnews.jsx
+++ src/pages/Cardnews.jsx
@@ -3,9 +3,9 @@ import ronaldo from '../resources/ronaldo.jpg'
 
 const Cardnews = () => {
   return (
-    <div className='mt-10'>
-      <img className=' h-72 w-96 opacity-70 z-10' src={ronaldo} alt="" />
-      <div className='w-30'>
+    <div className='mt-10 mr-11 bg-gray-600 w-80 rounded-[20px] py-3'>
+      <img className='mt-2 ml-4 h-72 w-72 opacity-70 z-10 rounded-[30px]' src={ronaldo} alt="" />
+      <div className='ml-4 w-72'>
         <h2 className='text-white text-lg font-semibold z-20 text-justify'>Lorem ipsum dolor sit amet consectetur adipisic</h2>
       </div>
     </div>
--- src/pages/News.jsx
+++ src/pages/News.jsx
@@ -1,24 +1,49 @@
 import React from 'react'
-import ronaldo from '../resources/ronaldo.jpg'
+import valverde from '../resources/valverde.jpg'
 import Cardnews from './Cardnews'
+import Footer from '../components/Footer'
 const News = () => {
   return (
     <>
       <div className='bg-black-400'>
-       <div className='text-white bg-gray-950 ml-11'>
-        <div className='flex mt-20 font-bold text-5xl'>
+       <div className='text-white bg-slate-900 ml-11 mr-11'>
+        <div className='text-white bg-gray-800 '>
+        <div className='flex mt-20 font-bold text-5xl p-10'>
           TRENDING NEWS
         </div>
         <div className='flex'>
-          <img className='opacity-70 z-10' src={ronaldo} alt="" />
-          <div className='mt-32 text-4xl text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate nihil omnis minima id pariatur. </div>
+          <img className='opacity-70 z-10 rounded-[30px] w-[650px] p-8' src={valverde} alt="" />
+          <div className='mt-32 text-3xl text-center'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Hic voluptate nihil omnis minima id pariatur. </div>
         </div>
-        <div className='flex'>
+        </div>
+        <div className=' mt-10 flex'>
+           <Cardnews/>
+           <Cardnews/>
+           <Cardnews/>
+           <Cardnews/>
+        </div>
+        <div className=' mt-10 flex'>
+           <Cardnews/>
+           <Cardnews/>
+           <Cardnews/>
+           <Cardnews/>
+        </div>
+        <div className=' mt-10 flex'>
+           <Cardnews/>
+           <Cardnews/>
+           <Cardnews/>
+           <Cardnews/>
+        </div>
+        <div className=' mt-10 flex'>
+           <Cardnews/>
            <Cardnews/>
            <Cardnews/>
            <Cardnews/>
         </div>
        </div>
+      </div>
+      <div className='mt-10'>
+        <Footer/>
       </div>
     </>
   )
